e4d29e03b3482bcd85739fdab47f30eb8c12161d
Yin Yang <84718589+Yinyang005@users.noreply.github.com>
2023-09-16 23:26:27 +0530

    Update Sum of  Odd.py

--- Task 3/Sum of  Odd.py
+++ Task 3/Sum of  Odd.py
@@ -1,7 +1,16 @@
+'''
+Question:-You are given a list. Return the sum of odd elements from the given list. 
+The input and output portions will be handled automatically. 
+You need to write a function with the recommended method signature.
+
+Constraints:-
+The list will have 1-100 elements.
+Each element will be an integer X where -100<=X<=100. 
+'''
 def Odd_Summation():
     sum = 0
-    to_cal = eval(input("Enter: "))
-    for num in to_cal:
+    to_calc = eval(input("Enter: "))
+    for num in to_calc:
         if num%2 != 0:
             sum += num
         else:
